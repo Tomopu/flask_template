d0946cd7bdd81255143adb3c187ab8568b134900
Tomopu <m.poochian@gmail.com>
2025-03-19 12:52:04 +0900

    feat: render_templateを使うように変更

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="images/favicon.ico")
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
